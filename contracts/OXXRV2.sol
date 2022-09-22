3a87ce69a222aa9387d99cee5a7e02ba7102cd4e
JBern <jehanmichel@gmail.com>
2022-09-22 14:18:44 +0200

    add DiamondNamed event. Edit safeMintWithName and setDiamondName functions by adding error messages and add emit event

--- contracts/OXXRV2.sol
+++ contracts/OXXRV2.sol
@@ -8,20 +8,26 @@ contract OXXRV2 is OXXR {
 
     mapping(uint256 => string) private diamondNames; 
 
+    /**
+     * @dev Emitted when `tokenId` token is nammed from `from` with the name `to`.
+     */
+    event DiamondNamed(address indexed from, uint256 indexed tokenId, string name);
+
     function safeMintWithName(address to, uint256 tokenId, string memory name) public virtual onlyRole(MINTER_ROLE) {
-        if(bytes(name).length > 0 ){
-            diamondNames[tokenId] = name;
-        }
+        require(bytes(name).length <= 100, "OXXR : string name is too long");
         _tokenIdCounter.increment();
+        diamondNames[tokenId] = name;
         _safeMint(to, tokenId);
+        emit DiamondNamed(msg.sender, tokenId, name);
     }
 
     function setDiamondName (uint256 tokenId, string memory name) public virtual{
-        require(bytes(name).length <= 100);
-        require(bytes(name).length > 0);
-        require(bytes(diamondNames[tokenId]).length == 0);
-        require(msg.sender == ownerOf(tokenId));
+        require(bytes(name).length <= 100, "OXXR : string name is too long"); 
+        require(bytes(name).length > 0, "OXXR : string name is empty");
+        require(bytes(diamondNames[tokenId]).length == 0, "OXXR: token already has a name");
+        require(msg.sender == ownerOf(tokenId), "AccessControl: sender account is not the token owner");
         diamondNames[tokenId] = name;
+        emit DiamondNamed(msg.sender, tokenId, name);
     }
 
     function  getDiamondName(uint256 tokenId) public view virtual returns(string memory){
